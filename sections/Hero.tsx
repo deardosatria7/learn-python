259a94d2044ece30f1e40336b44c3b2707c00158
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-02 08:00:46 +0700

    add pyscript

---
+++ sections/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="m-10">
+        <div className="">
+          <py-repl></py-repl>
+        </div>
+        <div>
+          <py-terminal></py-terminal>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
