c30fce7a5ab338e11c8e97f107945a2d9e2cc3e3
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-02 08:36:45 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import {Inter} from "next/font/google";
 import Hero from "@/sections/Hero";
+import Header from "@/sections/Header";
 
 const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
   return (
     <>
+      <Header></Header>
       <Hero />
     </>
   );
