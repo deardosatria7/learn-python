0a98ee8d7ea0e2d15370ad0167f84fc49ff0093f
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-02 08:00:33 +0700

    setting up project

---
+++ components/Layout.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const Layout = ({children}: any) => {
+  return (
+    <>
+      <Head>
+        <title>Learn python</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://pyscript.net/latest/pyscript.css"
+        />
+        <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+      </Head>
+      <section className="p-4">{children}</section>
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/themeChanger.tsx
@@ -0,0 +1,67 @@
+import {useState, useEffect} from "react";
+import {useTheme} from "next-themes";
+
+export default function themeChanger({children}: any) {
+  const {systemTheme, theme, setTheme} = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  const renderThemeChanger = () => {
+    const currTheme = theme === "system" ? systemTheme : theme;
+
+    if (currTheme === "dark") {
+      return (
+        <svg // bulb icon
+          className="h-7 w-7 hover:scale-110 cursor-pointer"
+          onClick={() => {
+            setTheme("light");
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.5}
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          />
+        </svg>
+      );
+    } else {
+      return (
+        <svg // moon icon
+          className="h-7 w-7 hover:scale-110 cursor-pointer"
+          onClick={() => {
+            setTheme("dark");
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.5}
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      );
+    }
+  };
+
+  return (
+    <>
+      <nav className="py-10 flex justify-between">
+        <div className="flex gap-5 justify-center">{children}</div>
+        <ul className="flex justify-center gap-2">
+          <li>{renderThemeChanger()}</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
