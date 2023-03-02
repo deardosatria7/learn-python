388718ab02bd867486342c195f41c6a407785333
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-02 08:19:30 +0700

    try to fix pyscript

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://pyscript.net/latest/pyscript.css"
+        />
+        <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,6 +1,8 @@
 const Hero = () => {
   return (
     <>
+      <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+      <script defer src="https://pyscript.net/latest/pyscript.js"></script>
       <div className="m-10">
         <div className="">
           <py-repl></py-repl>
