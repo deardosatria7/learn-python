cdf924409839464b7fdb758c4db6d321095d2e8d
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-03 20:46:58 +0700

    Update Editor.tsx

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -3,11 +3,17 @@ const Editor = () => {
     <>
       <div>
         <div className="pt-5 pr-5 pl-5 pb-3">
-          <div dangerouslySetInnerHTML={{__html: `<py-repl></py-repl>`}} />
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<py-repl>print ("Hello World!")</py-repl>`,
+            }}
+          />
         </div>
         <div className="pr-5 pl-5 pb-3">
           <div
-            dangerouslySetInnerHTML={{__html: `<py-terminal></py-terminal>`}}
+            dangerouslySetInnerHTML={{
+              __html: `<py-terminal></py-terminal>`,
+            }}
           />
         </div>
       </div>
