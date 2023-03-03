fd7ec509593a60f49b6fde13adafc0b9c518eb49
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-03 19:02:26 +0700

    Update Header.tsx

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -4,11 +4,13 @@ const Header = ({children}: any) => {
   return (
     <>
       <nav className="p-4">
-        <div className="flex justify-between items-center">
-          <div className="flex gap-5 justify-center">{children}</div>
-          <ul className="flex justify-center gap-2">
-            <li>{renderThemeChanger()}</li>
-          </ul>
+        <div className="flex justify-between">
+          <div>{children}</div>
+          <div>
+            <ul className="flex justify-center gap-2">
+              <li>{renderThemeChanger()}</li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
