b55a72f1670d6b1cfa0cc289784d70371b1f97d0
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-03 20:07:34 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,19 @@ import Hero from "@/sections/Hero";
 import Header from "@/sections/Header";
 import Link from "next/link";
 
-const inter = Inter({subsets: ["latin"]});
-
 export default function Home() {
   return (
     <>
       <Header>
         <div>
-          <h4 className="text-lg text-neutral-400">Belajar-Python</h4>
+          <h4 className="text-lg dark:text-neutral-400">Belajar-Python</h4>
         </div>
       </Header>
       <Hero>
         <div className="mt-20">
-          <div className="flex flex-col bg-neutral-800 py-10 px-4 rounded-md">
+          <div className="flex flex-col dark:bg-neutral-800 py-10 px-4 rounded-md">
             <div className="">
-              <h2 className="text-md text-neutral-400">Selamat Datang!</h2>
+              <h2 className="text-md dark:text-neutral-400">Selamat Datang!</h2>
             </div>
             <div className="pt-2">
               <div className="text-xl text-blue-300 font-semibold font-sans">
@@ -25,7 +23,7 @@ export default function Home() {
               </div>
               <div className="flex items-center pt-8">
                 <Link href={"/playground"} legacyBehavior>
-                  <div className="px-4 py-2 bg-slate-700 hover:bg-slate-500 rounded-lg">
+                  <div className="px-4 py-2 bg-slate-700 hover:bg-slate-500 text-white rounded-lg">
                     Coba Python
                   </div>
                 </Link>
