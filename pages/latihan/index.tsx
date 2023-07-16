9e643736cdf0ee5fad5b4baab024189b6c9fef34
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-07-16 18:42:59 +0700

    update page route

--- pages/latihan/index.tsx
+++ pages/latihan/index.tsx
@@ -4,8 +4,11 @@ const latihan = () => {
   return (
     <>
       <h1>Ini adalah page latihan</h1>
-      <h3>
+      <h3 className="flex flex-col">
         <Link href={"../"}>Back to home</Link>
+        <Link href={"/latihan/latihan1"} className="text-blue-600">
+          Ke Latihan 1
+        </Link>
       </h3>
     </>
   );
--- pages/latihan/latihan1.tsx
+++ pages/latihan/latihan1.tsx
@@ -4,8 +4,11 @@ const latihan1 = () => {
   return (
     <>
       <h1>Ini adalah page latihan 1</h1>
-      <h3>
+      <h3 className="flex flex-col">
         <Link href={"../"}>Back to home</Link>
+        <Link href={"../latihan"} className="text-blue-600">
+          Ke halaman latihan
+        </Link>
       </h3>
     </>
   );
