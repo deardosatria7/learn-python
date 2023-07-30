a10d2e4f13a01a6f6124327a7e3bedb9e7124546
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-07-30 15:21:42 +0700

    update page latihan

--- pages/latihan/index.tsx
+++ pages/latihan/index.tsx
@@ -9,13 +9,19 @@ const latihan = () => {
           <h4 className="text-lg text-white">Belajar-Python</h4>
         </div>
       </Header>
-      <h1>Ini adalah page latihan</h1>
-      <h3 className="flex flex-col">
-        <Link href={"../"}>Back to home</Link>
-        <Link href={"/latihan/latihan1"} className="text-blue-600">
-          Ke Latihan 1
-        </Link>
-      </h3>
+      <section className="p-4">
+        <h1 className="text-2xl font-semibold">Pengenalan Python</h1>
+        <p className="pt-3 pl-1">
+          Python adalah salah satu bahasa pemrograman yang dikenalkan pada tahun
+          19XX oleh Guido Van Rossum.
+        </p>
+        <h3 className="pt-4 flex flex-col">
+          <Link href={"../"}>Back to home</Link>
+          <Link href={"/latihan/latihan1"} className="text-blue-600">
+            Ke Latihan 1
+          </Link>
+        </h3>
+      </section>
     </>
   );
 };
