eed8955c505e34c5fe741f176f0448b87664291c
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-09-20 14:32:15 +0700

    update content latihan

--- pages/latihan/operator_1.tsx
+++ pages/latihan/operator_1.tsx
@@ -14,31 +14,27 @@ const pageOperator = () => {
       </Header>
       <section className="p-4">
         <h1 className="text-2xl font-semibold">Operator Python</h1>
-        <p className="pt-3 pl-1">Operator dasar dalam python.</p>
         <p className="pt-3 pl-1">
           Selamat! Kalian telah mempelajari tipe-tipe variabel dalam python.
         </p>
+        <p className="pt-3 pl-1">
+          Selanjutnya adalah materi mengenai operator dalam python. Di dalam
+          python, ada beberapa operator yang dapat dimasukkan kedalam kode.
+          Contohnya adalah <b>Operator Aritmatika</b>. Beberapa operator
+          aritmatika termasuk penjumlahan, pengurangan, perkalian, pembagian,
+          dan perpangkatan.
+        </p>
         <div className="pt-3 pl-1">
-          <li>
-            <i>Integer</i> yang berupa angka bulat.
-          </li>
-          <li>
-            <i>Float</i> yang berupa angka desimal.
-          </li>
-          <li>
-            <i>String</i> yang berupa teks.
-          </li>
-          <li>
-            <i>Boolean</i> yang bernilai True atau False.
-          </li>
+          <li>Penjumlahan (+)</li>
+          <li>Pengurangan (-)</li>
+          <li>Perkalian (*)</li>
+          <li>Pembagian (/)</li>
+          <li>Perpangkatan (**)</li>
+          <li>Pembagian hasil bulat (//)</li>
         </div>
         <div className="pt-3 pl-1">
-          Coba kalian isi variabel di bawah ini sesuai kreativitas kalian!
-          <br />
-          <span className="italic text-blue-600">
-            Output hanya akan keluar ketika telah diisi seluruh variabel
-            dibawah.
-          </span>
+          Coba kalian jalankan kode di bawah ini dan perhatikan mengapa hasilnya
+          seperti itu!
         </div>
         <Editor>
           <div
@@ -48,18 +44,12 @@ const pageOperator = () => {
               terminal = false
               </py-config>
               <py-repl output="replOutput">
-              string_namaProduk = ""
-              integer_harga = 
-              float_jumlah = 
-              boolean_statusTersedia = 
-
-              # jangan edit kode dibawah ini
-              harga_format = "{:,}".format(integer_harga)
-              print ("Berikut adalah keterangan dari produk diatas")
-              print ("Merek: " + string_namaProduk)
-              print ("Harga: Rp", harga_format)
-              print ("Jumlah: ", float_jumlah)
-              print ("Status tersedia: ", boolean_statusTersedia)
+              print (10 + 3)
+              print (10 - 3)
+              print (10 * 3)
+              print (10 / 3)
+              print (10 ** 3)
+              print (10 // 3)
               </py-repl>`,
             }}
           />
@@ -67,13 +57,13 @@ const pageOperator = () => {
         <div className="flex">
           <h3 className="flex gap-2 pt-4">
             <Link
-              href={"/latihan/variabel_4"}
+              href={"/latihan/variabel_5"}
               className="text-white bg-red-500 hover:bg-red-400 px-4 py-2 rounded-md"
             >
               Sebelumnya
             </Link>
             <Link
-              href={"/latihan/variabel_6"}
+              href={"/latihan/operator_2"}
               className="text-white bg-blue-600 hover:bg-blue-500 px-4 py-2 rounded-md"
             >
               Selanjutnya
