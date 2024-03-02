e5b8fbc6fc85fe5d64dd22b024ebb18a518df5b0
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2024-03-02 20:33:51 +0700

    add meta description

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,6 +7,11 @@ const Layout = ({children}: any) => {
         <title>Belajar python</title>
         <link rel="icon" type="image/png" href="../favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Website belajar python gratis dengan built-in-compiler Python"
+        />
+        <meta name="keywords" content="belajar, python, gratis, indonesia" />
       </Head>
       <section>{children}</section>
     </>
