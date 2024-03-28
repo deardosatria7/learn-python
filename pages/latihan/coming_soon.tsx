9dbf72dbfca096a0243e062074e1d784d9924b58
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2024-03-28 15:32:17 +0700

    Create coming_soon.tsx

---
+++ pages/latihan/coming_soon.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const ComingSoon = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center align-middle h-screen p-4 text-center">
+        <div className="text-4xl font-bold">Coming Soon</div>
+        <div className="mt-4">
+          Sedang dalam pengembangan, terimakasih telah mencapai akhir dari
+          website ini.
+        </div>
+      </div>
+    </>
+  );
+};
+export default ComingSoon;
